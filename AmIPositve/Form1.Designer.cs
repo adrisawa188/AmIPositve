647e50af5d2e3b071a3ddc7da5e058ff4f4e8afc
Adrian Sawatzky <adrisawa188@ed.amdsb.ca>
2021-05-06 08:45:25 -0400

    Add project files.

---
+++ AmIPositve/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace AmIPositve
+{
+    partial class ifStaments
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputIntLabel = new System.Windows.Forms.Label();
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.enterButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // inputIntLabel
+            // 
+            this.inputIntLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputIntLabel.Location = new System.Drawing.Point(51, 44);
+            this.inputIntLabel.Name = "inputIntLabel";
+            this.inputIntLabel.Size = new System.Drawing.Size(143, 29);
+            this.inputIntLabel.TabIndex = 0;
+            this.inputIntLabel.Text = "Input an Integer";
+            // 
+            // inputBox
+            // 
+            this.inputBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputBox.Location = new System.Drawing.Point(200, 44);
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(100, 29);
+            this.inputBox.TabIndex = 1;
+            // 
+            // enterButton
+            // 
+            this.enterButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.enterButton.Location = new System.Drawing.Point(200, 79);
+            this.enterButton.Name = "enterButton";
+            this.enterButton.Size = new System.Drawing.Size(100, 46);
+            this.enterButton.TabIndex = 2;
+            this.enterButton.Text = "Enter";
+            this.enterButton.UseVisualStyleBackColor = true;
+            this.enterButton.Click += new System.EventHandler(this.enterButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.Location = new System.Drawing.Point(51, 170);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(249, 193);
+            this.outputLabel.TabIndex = 3;
+            // 
+            // ifStaments
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(359, 408);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.enterButton);
+            this.Controls.Add(this.inputBox);
+            this.Controls.Add(this.inputIntLabel);
+            this.Name = "ifStaments";
+            this.Text = "If Statements";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label inputIntLabel;
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.Button enterButton;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
---
+++ AmIPositve/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AmIPositve
+{
+    public partial class ifStaments : Form
+    {
+        double integer;
+        double divideInt;
+        public ifStaments()
+        {
+            InitializeComponent();
+        }
+
+        private void enterButton_Click(object sender, EventArgs e)
+        {
+            divideInt = integer / 7; 
+
+            integer = Convert.ToInt32(inputBox.Text);
+
+            if(integer > 0)
+            {
+                outputLabel.Text = $"{integer} is a postive number";
+            }
+            else
+            {
+                outputLabel.Text = $"{integer} is not a postive number";
+            }
+
+            if (integer % 7 == 0)
+            {
+                outputLabel.Text += $"\n\n{integer} is divisible by 7"; 
+            }
+            else
+            {
+                outputLabel.Text += $"\n\n{integer} is not divivble by 7"; 
+            }
+
+        }
+    }
+}
